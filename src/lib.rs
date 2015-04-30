285d61cebe01c76c707900110b400f1a497719bb
Keenan Lidral-Porter <klp2012@gmail.com>
2015-04-29 23:41:22 -0700

    errors but at least they're logical errors

--- src/lib.rs
+++ src/lib.rs
@@ -4,29 +4,31 @@ pub fn is_prime(x: i32) -> bool {
     }
 
     let upper_limit: i32 = (x as f64).sqrt().ceil() as i32;
-    fn is_odd(&i: i32) -> bool { i % 2 == 1 }
-    let mut sieve = (1..upper_limit).filter(is_odd).collect::<Vec<i32>>();
+    let mut sieve = (1..upper_limit).filter(|x| { x % 2 == 1 }).collect::<Vec<i32>>();
 
-    let result: bool;
-    for num in &sieve {
-        result = prime_test(&num, &x, &sieve);
+    let mut result: bool;
+    for num in sieve {
+       result = prime_test(num, &x, &mut sieve);
+        if result {
+            break;
+        }
     }
     result
 }
 
-pub fn prime_test(&num: i32, &x: i32, &mut sieve) -> bool {
+pub fn prime_test(num: i32, x: &i32, sieve: &mut Vec<i32>) -> bool {
     // check if this specific number in sieve is prime
-    for i in (1..&num).collect::<Vec<_>>() {
-        if &num % i != 0 {
+    for i in (1..num).collect::<Vec<i32>>() {
+        if num % i != 0 {
             // if it's not a prime, remove it and all multiples from sieve
-            for multiple in (i..&x + 1).step_by(i).collect::<Vec<_>>(); {
-                if multiple == &x {
+            for multiple in (i..x + 1).step_by(i).collect::<Vec<i32>>() {
+                if multiple == *x {
                     return true
                 }
-                &sieve.position(multiple).map(|element| &sieve.remove(element));
+                sieve.iter().position(|x| *x == multiple).map(|element| &sieve.remove(element));
             }
         } else {
-            if &x % i == 0 {
+            if x % i == 0 {
                 return true;
             }
         }
