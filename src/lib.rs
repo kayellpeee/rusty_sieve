e8090619d6e556fb9435152d4bec6791866fc70c
Keenan Lidral-Porter <klp2012@gmail.com>
2015-05-04 00:35:41 -0700

    creates "sieve" for range of numbers by running prime_test on all no's

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-pub fn is_prime(x: i32) -> bool {
+#![feature(step_by)]
+pub fn is_prime(x: &i32) -> bool {
    // Handle small edge cases first
-    if x == 1 || x == 2 {
+    if *x == 2 {
         return true;
-    } else if x == 0 {
+    } else if *x == 0 || *x == 1 {
         return false
     }
 
     // a number is prime if it is only divisible by 1 & itself
     // once we pass the sqrt of a number, we don't need to proceed checking for divisors
-    let upper_limit: i32 = (x as f64).sqrt().ceil() as i32;
+    let upper_limit: i32 = (*x as f64).sqrt().ceil() as i32;
 
-    for i in 2..upper_limit {
-        if x % i == 0 {
+    for i in (2..upper_limit + 1) {
+        if *x % i == 0 {
             return false;
         }
     }
@@ -21,28 +22,39 @@ pub fn is_prime(x: i32) -> bool {
 pub fn prime_sieve(start: i32, end: i32) -> Vec<i32> {
     // all numbers in the sieve should be prime
     // start at the first prime and remove all of it's multiples
-    let sieve = (start..end).collect::<Vec<_>>();
+    let mut sieve = (start..end).collect::<Vec<_>>();
+    for num in start..end {
+        if !is_prime(&num) {
+                let index = sieve.binary_search(&num).unwrap();
+                sieve.remove(index);
+        }
+    }
     sieve
 }
 
 #[test]
 fn basic_prime() {
-    assert!(is_prime(3));
-    assert!(is_prime(5));
-    assert!(!is_prime(8));
+    assert!(is_prime(&3));
+    assert!(is_prime(&5));
+    assert!(!is_prime(&8));
 }
 #[test]
 fn double_digits(){
-    assert!(is_prime(97));
-    assert!(is_prime(17));
-    assert!(!is_prime(40));
-    assert!(!is_prime(39));
+    assert!(is_prime(&97));
+    assert!(is_prime(&17));
+    assert!(!is_prime(&40));
+    assert!(!is_prime(&39));
 }
 #[test]
 fn triple_digits() {
-    assert!(!is_prime(100));
+    assert!(!is_prime(&100));
+}
+#[test]
+fn sieve_to_ten() {
+    assert_eq!(prime_sieve(0, 10), vec![2, 3, 5, 7]);
 }
 #[test]
 fn sieve_to_hunnid(){
-    assert_eq!(prime_sieve(0, 100), (0..100).filter(|x| is_prime(*x)).collect::<Vec<_>>());
+    assert_eq!(prime_sieve(0, 100), vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]); 
 }
